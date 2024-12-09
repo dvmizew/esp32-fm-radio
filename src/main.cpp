0a087befb4e361a55c2fce3075483586bb44048a
dvmizew <dvmiradu@gmail.com>
2024-12-09 23:29:09 +0200

    formatted the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
 #include <Arduino.h>
+#include "lvgl.h"
 #include "hardware.h"
 #include "displayHandle.h"
 #include "radioHandle.h"
 #include "bluetooth.h"
+#include "wifiHandle.h"
 #include "webserver.h"
+#include "mainMenu.h"
 
 void setup() {
     Serial.begin(115200);
     setupHardware();
-    initSPIFFS();
-    initDisplay();
-    initRadio();
+    
+    // display stuff
+    lv_init(); // initialize LittlevGL
+    initDisplay(); // initialize OLED display
+
+    initRadio(); // initialize radio module
     setupBluetooth();
+    // web server stuff
     setupWebServer();
-    updateDisplay();
+    initSPIFFS(); // init SPIFFS for web server
+    
+    initMainMenu();
 }
 
 void loop() {
     handleJoystickInput();
-    handleRadioControl();
-    playThroughBluetoothSpeaker();
+    handleMainMenu();
 }
