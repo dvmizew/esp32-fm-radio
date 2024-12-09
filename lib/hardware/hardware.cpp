a63f5432ea17fafe9b3dda175273fc173a0ae994
dvmizew <dvmiradu@gmail.com>
2024-12-09 23:25:54 +0200

    small changes to device handling

--- lib/hardware/hardware.cpp
+++ lib/hardware/hardware.cpp
@@ -1,100 +1,27 @@
-#include <WiFi.h>
+#include <Wire.h>
+#include <TEA5767.h>
 #include "hardware.h"
 #include "radioHandle.h"
 #include "bluetooth.h"
 
+TwoWire I2C_display = TwoWire(1); // we use I2C1 for the display
+Adafruit_SSD1306 display(128, 64, &I2C_display); // OLED display object
+TEA5767 radio;
+
 void setupHardware() {
-    pinMode(BUTTON_PLAY, INPUT_PULLUP);
-    pinMode(JOYSTICK_SW, INPUT_PULLUP);
-    Wire.begin(DISPLAY_SDA_PIN, DISPLAY_SCL_PIN);
-}
+    // Wire is for TEA5767 radio
+    // I2C_display is for OLED display
+    Wire.begin(); // radio init
+    Wire.setClock(100000); // radio clock
 
-void showAvailableWiFiNetworks() {
-    Serial.println("Scanning for available Wi-Fi networks...");
+    I2C_display.begin(DISPLAY_SDA_PIN, DISPLAY_SCL_PIN);
 
-    WiFi.mode(WIFI_STA); // set ESP32 to station mode
-    WiFi.disconnect(); // disconnect from any other network
-    delay(100);
-
-    int networkCount = WiFi.scanNetworks(); // scan for available networks
-    Serial.println("Scan done");
-
-    if (networkCount == 0)
-        Serial.println("No networks found");
-
-    else {
-        Serial.printf("%d networks found\n", networkCount);
-        Serial.println("Nr | SSID                             | RSSI | CH | Encryption");
-        for (int i = 0; i < networkCount; ++i) {
-            Serial.printf("%2d | %-32.32s | %4d | %2d | ", i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i), WiFi.channel(i));
-            switch (WiFi.encryptionType(i)) {
-                case WIFI_AUTH_OPEN:
-                    Serial.print("open");
-                    break;
-                case WIFI_AUTH_WEP:
-                    Serial.print("WEP");
-                    break;
-                case WIFI_AUTH_WPA_PSK:
-                    Serial.print("WPA");
-                    break;
-                case WIFI_AUTH_WPA2_PSK:
-                    Serial.print("WPA2");
-                    break;
-                case WIFI_AUTH_WPA_WPA2_PSK:
-                    Serial.print("WPA+WPA2");
-                    break;
-                case WIFI_AUTH_WPA2_ENTERPRISE:
-                    Serial.print("WPA2-EAP");
-                    break;
-                case WIFI_AUTH_WPA3_PSK:
-                    Serial.print("WPA3");
-                    break;
-                case WIFI_AUTH_WPA2_WPA3_PSK:
-                    Serial.print("WPA2+WPA3");
-                    break;
-                case WIFI_AUTH_WAPI_PSK:
-                    Serial.print("WAPI");
-                    break;
-                default:
-                    Serial.print("unknown");
-                    break;
-            }
-            Serial.println();
-            delay(10);  // delay to allow serial monitor to process output
-        }
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SSD1306_I2C_ADDRESS)) {
+        Serial.println(F("Error initializing display!"));
+        while (true);
     }
 
-    WiFi.scanDelete(); // free memory used by scan results
-}
-
-void connectToWifi(const char *ssid, const char *password) {
-    WiFi.begin(ssid, password);
-    Serial.println("Connecting to WiFi...");
-
-    unsigned long startAttemptTime = millis();
-    int retryCount = 0;
-    const int maxRetries = 5;
-    const int retryDelay = 2000;
-
-    while (WiFi.status() != WL_CONNECTED && retryCount < maxRetries) {
-        // check if connection is successful
-        if (WiFi.status() == WL_CONNECTED) {
-            Serial.println("Connected!");
-            Serial.print("IP Address: ");
-            Serial.println(WiFi.localIP());
-            Serial.println("MAC Address: ");
-            Serial.println(WiFi.macAddress());
-            return; // success
-        }
-        // retry connection
-        retryCount++;
-        Serial.printf("Connection attempt %d of %d...\n", retryCount, maxRetries);
-        delay(retryDelay);
-        if (retryCount >= maxRetries) {
-            Serial.println("Connection failed, max retries reached.");
-            return;
-        }
-    }
+    radio.init();
 }
 
 unsigned long lastDebounceTime = 0;
--- lib/hardware/hardware.h
+++ lib/hardware/hardware.h
@@ -3,27 +3,35 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+#include <Adafruit_SSD1306.h>
 
 #define debounceDelay 50
 
 // pin definitions
-#define BUTTON_PLAY 33
+#define BUTTON_PLAY 0 // inca nu e definit
 
 // joystick pins
-#define JOYSTICK_X 34
-#define JOYSTICK_Y 35
-#define JOYSTICK_SW 32
+#define JOYSTICK_X 9 // SD2
+#define JOYSTICK_Y 10 // SD3
+#define JOYSTICK_SW 11 // CMD
 
 // OLED display pins
 #define DISPLAY_SDA_PIN 21
 #define DISPLAY_SCL_PIN 22
 
 // MAX98357A amplifier pins
-#define AMP_SCK 14
-#define AMP_MISO 12
-#define AMP_MOSI 13
-#define AMP_CS 15
-#define AMP_SDA 26
+#define AMP_SCK 13 // Clock
+// #define AMP_MISO 12
+// #define AMP_MOSI 11
+#define AMP_CS 12 // Chip select
+#define AMP_SDA 39 // SN
+
+// TEA5767 radio pins
+#define TEA5767_SDA 17
+#define TEA5767_SCL 16
+
+extern TwoWire I2C_display;
+extern Adafruit_SSD1306 display;
 
 void setupHardware();
 void showAvailableWiFiNetworks();
