238e67a5377973cd1784fdef87a84232e27ab043
dvmizew <dvmiradu@gmail.com>
2024-12-26 23:06:00 +0200

    changed pin conf once again

--- lib/hardware/hardware.h
+++ lib/hardware/hardware.h
@@ -1,28 +1,38 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
+#include <Wire.h>
+#include <Arduino.h>
+#include <driver/i2s.h>
+
 // pin definitions
-#define BUTTON_PLAY 0 // inca nu e definit
+// #define BUTTON_PLAY 0
+#define INTERNAL_LED 2
 
 // joystick pins
 #define JOYSTICK_X 27
 #define JOYSTICK_Y 26
 #define JOYSTICK_SW 32
 
-// OLED display pins
+// OLED display pins (I2C)
 #define DISPLAY_SDA_PIN 21
 #define DISPLAY_SCL_PIN 22
 #define SSD1306_I2C_ADDRESS 0x3C
 
-// MAX98357A amplifier pins
-#define AMP_SCK 13 // Clock
-// #define AMP_MISO 12
-// #define AMP_MOSI 11
-#define AMP_CS 12 // Chip select
-#define AMP_SDA 25
+// MAX98357A amplifier pins (I2S)
+#define AMP_LRC 25 // LRCLK
+#define AMP_BLCK 13 // Clock
+#define AMP_DIN 12 // Data in
+#define AMP_GAIN 14
+#define AMP_SD 33
 
-// TEA5767 radio pins
+extern i2s_config_t amp_i2s_config;
+extern i2s_pin_config_t amp_config;
+
+// TEA5767 radio pins (I2C)
 #define TEA5767_SDA 17
 #define TEA5767_SCL 16
+
+void printI2CDevices();
 
 #endif
