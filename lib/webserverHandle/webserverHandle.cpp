59fe3588331fdced1cbde42e8fe6dfb395951a64
dvmizew <dvmiradu@gmail.com>
2025-01-01 19:15:36 +0200

    separated the wifi credentials

--- lib/webserverHandle/webserverHandle.cpp
+++ lib/webserverHandle/webserverHandle.cpp
@@ -4,7 +4,7 @@ AsyncWebServer server(80);
 
 void connectToWifi() {
     if (!isWiFiConnected()) {
-        connectToWiFiNetwork("ssid", "password"); // replace with your Wi-Fi credentials
+        connectToWiFiNetwork(SSID, PASSWORD); // replace with your own wifi credentials in lib/wifiHandle/wifiCredentials.h
     }
     Serial.printf_P(PSTR("\nConnected to Wi-Fi\nClick here to open the web interface: http://%s\n\n"), WiFi.localIP().toString().c_str());
 }
---
+++ lib/wifiHandle/wifiCredentials.h
@@ -0,0 +1,4 @@
+// replace with your own wifi credentials
+
+#define SSID "ssid"
+#define PASSWORD "password"
