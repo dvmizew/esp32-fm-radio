bea6547daed1d2ac0fd07d4c8e550e9040002fff
dvmizew <dvmiradu@gmail.com>
2025-01-01 23:16:42 +0200

    documented the code and did some fixes

--- lib/bluetooth/bluetooth.cpp
+++ lib/bluetooth/bluetooth.cpp
@@ -6,75 +6,80 @@ BluetoothA2DPSink btAudioSink(i2s); // use ESP32 as speaker
 // Audio audio; // for tone control
 // BluetoothA2DPSource btAudioSource; // play through an external speaker
 bool connected = false;
+bool bluetoothSpeakerInitialized = false;
 const char* deviceName = DEVICE_NAME;
 
 void initializeBluetoothSpeaker() {
-    Serial.begin(115200);
+    if (!bluetoothSpeakerInitialized) {
+        Serial.begin(115200);
 
-    // old way, sounds better but crashes after a while
-    // audio.setPinout(AMP_BLCK, AMP_LRC, AMP_DIN);
-    // audio.setVolume(100);
+        // old way, sounds better but crashes after a while
+        // audio.setPinout(AMP_BLCK, AMP_LRC, AMP_DIN);
+        // audio.setVolume(100);
 
-    // initialize I2SStream
-    auto cfg = i2s.defaultConfig();
-    cfg.pin_bck = AMP_BLCK;
-    cfg.pin_ws = AMP_LRC;
-    cfg.pin_data = AMP_DIN;
-    cfg.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT;
-    cfg.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT;
-    cfg.use_apll = false;
-    i2s.begin(cfg);
+        // initialize I2SStream
+        auto cfg = i2s.defaultConfig(); // get the default configuration for I2S
+        cfg.pin_bck = AMP_BLCK; // set the bit clock pin
+        cfg.pin_ws = AMP_LRC; // set the word select pin
+        cfg.pin_data = AMP_DIN; // set the data pin
+        cfg.bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT; // set the bits per sample
+        cfg.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT; // set the channel format
+        cfg.use_apll = false;
+        i2s.begin(cfg);
 
-    // hopefully this will make the sound better
-    // set it to 0 to disable, 100 to max
-    // const int8_t gainLowPass = 5;   // increase bass
-    // const int8_t gainBandPass = 3;  // mid-range
-    // const int8_t gainHighPass = 4;  // increase treble
-    // audio.setTone(gainLowPass, gainBandPass, gainHighPass);
+        // hopefully this will make the sound better
+        // set it to 0 to disable, 100 to max
+        // const int8_t gainLowPass = 5;   // increase bass
+        // const int8_t gainBandPass = 3;  // mid-range
+        // const int8_t gainHighPass = 4;  // increase treble
+        // audio.setTone(gainLowPass, gainBandPass, gainHighPass);
 
-    btAudioSink.start(deviceName);
+        btAudioSink.start(deviceName); // start the Bluetooth audio sink and now it's discoverable
 
-    // getting metadata from A2DP source
-    btAudioSink.set_avrc_metadata_callback([](uint8_t id, const uint8_t *value) {
-        if (value == nullptr || strlen((const char *)value) == 0) {
-            return;
-        }
-
-        switch (id) {
-            case ESP_AVRC_MD_ATTR_TITLE:
-                Serial.print(F("Title: "));
-                break;
-            case ESP_AVRC_MD_ATTR_ARTIST:
-                Serial.print(F("Artist: "));
-                break;
-            case ESP_AVRC_MD_ATTR_ALBUM:
-                Serial.print(F("Album: "));
-                break;
-            case ESP_AVRC_MD_ATTR_GENRE:
-                Serial.print(F("Genre: "));
-                break;
-            case ESP_AVRC_MD_ATTR_PLAYING_TIME:
-                Serial.print(F("Playing time: "));
-                break;
-            case ESP_AVRC_MD_ATTR_TRACK_NUM:
-                Serial.print(F("Track number: "));
-                break;
-            default:
+        // getting metadata from A2DP source (external device)
+        btAudioSink.set_avrc_metadata_callback([](uint8_t id, const uint8_t *value) {
+            if (value == nullptr || strlen((const char *)value) == 0) {
                 return;
-        }
-        Serial.println((const char *)value);
-    });
+            }
 
-    btAudioSink.set_avrc_connection_state_callback(connectionStateCallback);
-    
-    btAudioSink.set_auto_reconnect(true, 1000); // auto reconnect after 1 second
-    Serial.printf_P(PSTR("%s is now discoverable\n"), DEVICE_NAME);
+            switch (id) {
+                case ESP_AVRC_MD_ATTR_TITLE:
+                    Serial.print(F("Title: "));
+                    break;
+                case ESP_AVRC_MD_ATTR_ARTIST:
+                    Serial.print(F("Artist: "));
+                    break;
+                case ESP_AVRC_MD_ATTR_ALBUM:
+                    Serial.print(F("Album: "));
+                    break;
+                case ESP_AVRC_MD_ATTR_GENRE:
+                    Serial.print(F("Genre: "));
+                    break;
+                case ESP_AVRC_MD_ATTR_PLAYING_TIME:
+                    Serial.print(F("Playing time: "));
+                    break;
+                case ESP_AVRC_MD_ATTR_TRACK_NUM:
+                    Serial.print(F("Track number: "));
+                    break;
+                default:
+                    return;
+            }
+            Serial.println((const char *)value);
+        });
+
+        btAudioSink.set_avrc_connection_state_callback(connectionStateCallback);
+        btAudioSink.set_auto_reconnect(true, 1000); // auto reconnect after 1 second
+        Serial.printf_P(PSTR("%s is now discoverable\n"), DEVICE_NAME);
+        bluetoothSpeakerInitialized = true;
+    }
 }
 
 void deinitializeBluetoothSpeaker() {
+    Serial.println(F("Stopping Bluetooth audio sink..."));
     btAudioSink.stop();
+    Serial.println(F("Stopping I2S..."));
     i2s.end();
-    Serial.end();
+    Serial.println(F("Bluetooth speaker deinitialized"));
 }
 
 void disconnectBluetoothSinkDevice() {
@@ -101,7 +106,7 @@ void togglePlayback() {
 void volumeUp() {
     int volume = btAudioSink.get_volume();
     if (volume < 100) {
-        volume += 5;
+        volume += VOL_STEP;
         btAudioSink.set_volume(volume);
     }
 }
@@ -109,7 +114,7 @@ void volumeUp() {
 void volumeDown() {
     int volume = btAudioSink.get_volume();
     if (volume > 0) {
-        volume -= 5;
+        volume -= VOL_STEP;
         btAudioSink.set_volume(volume);
     }
 }
